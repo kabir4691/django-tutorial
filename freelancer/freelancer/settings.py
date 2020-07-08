d90f64bf97836bb62e4e07e9120525b061cabe39
Arkesh Jaiswal <aj.xddx@gmail.com>
2020-07-08 20:29:07 +0530

    COntact

--- freelancer/freelancer/settings.py
+++ freelancer/freelancer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'web.apps.WebConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- freelancer/web/views.py
+++ freelancer/web/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Contact
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {'company_name': 'Freelancer'})
+
+def contact(request):
+    contact = Contact(
+        name = request.POST["name"],
+        email = request.POST["email"],
+        phone = request.POST["phone"],
+        message = request.POST["message"]
+    )
+    contact.save()
+    return JsonResponse({
+        'success': True,
+        'message': 'Your query has been recorded. We will get in touch with you shortly.',
+        'data': {
+            'name': request.POST["name"],
+            'email': request.POST["email"],
+            'phone': request.POST["phone"],
+            'message': request.POST["message"]
+        }
+    }, json_dumps_params={'indent': 2})
