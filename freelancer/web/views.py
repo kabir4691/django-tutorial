e2efdfe698c9c1a6199d193fee097c636a20e8ce
Arkesh Jaiswal <aj.xddx@gmail.com>
2020-07-09 19:28:09 +0530

    posting through js

--- freelancer/web/views.py
+++ freelancer/web/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Contact
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {'company_name': 'Freelancer'})
 
 def contact(request):
+    data = json.loads(request.body)
+    # print(json.loads('{"name": "Arkesh"}'))
+    # print(json.loads(request.body))
     contact = Contact(
-        name = request.POST["name"],
-        email = request.POST["email"],
-        phone = request.POST["phone"],
-        message = request.POST["message"]
+        name = data["name"],
+        email = data["email"],
+        phone = data["phone"],
+        message = data["message"]
     )
     contact.save()
     return JsonResponse({
         'success': True,
         'message': 'Your query has been recorded. We will get in touch with you shortly.',
-        'data': {
-            'name': request.POST["name"],
-            'email': request.POST["email"],
-            'phone': request.POST["phone"],
-            'message': request.POST["message"]
-        }
+        'data': data
     }, json_dumps_params={'indent': 2})
